010280756f32bdd227cf0de7d18eaaa2e5695e68
Saurabh Bohra <thisissaurabhbohra@gmail.com>
2020-06-17 17:05:43 +0530

    Initial Commit

---
+++ SampleProject/SampleProject/SampleProject/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SampleProject
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+    }
+}
---
+++ SampleProject/SampleProject/SampleProject/MainPageViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace SampleProject
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            ExecuteGridCommand = new Command(ExecuteGridMethod);
+            ExecuteButtonCommand = new Command(ExecuteButtonMethod);
+        }
+
+        private int _gridExecutionCount;
+        public int GridExecutionCount
+        {
+            get => _gridExecutionCount;
+            set
+            {
+                _gridExecutionCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _buttonExecutionCount;
+        public int ButtonExecutionCount
+        {
+            get => _buttonExecutionCount;
+            set
+            {
+                _buttonExecutionCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command ExecuteGridCommand { get; set; }
+
+        public Command ExecuteButtonCommand { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public void ExecuteGridMethod()
+        {
+            GridExecutionCount++;
+        }
+
+        public void ExecuteButtonMethod()
+        {
+            ButtonExecutionCount++;
+        }
+    }
+}
